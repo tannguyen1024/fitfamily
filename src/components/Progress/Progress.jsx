da5dbdf4db29df73748318ee2457daba855a356e
Tan Nguyen <tannguyen1024@gmail.com>
2020-07-14 16:43:44 -0500

    Updated Workout page

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import styles from '../Style/Style';
-import { Box } from '@material-ui/core'
+import { Box, Paper, Table, TableBody, TableCell, TableHead, TableContainer, TablePagination, TableRow } from '@material-ui/core'
 // Sweet Alert 2 //
 import Swal from 'sweetalert2/src/sweetalert2.js';
 import '../Style/Swal.scss';
@@ -12,13 +12,47 @@ import '../Style/Swal.scss';
 import Header from '../Header/Header';
 
 class Progress extends Component {
+
+ createData = (name, date, weight) => {
+     return { name, date, weight };
+}
+    
     render() {
         const {classes} = this.props;
+        const rows = [
+            this.createData('Tan', 'Jul 12, 2020', 196),
+            this.createData('Linda', 'Jul 12, 2020', 182.6),
+        ];
+
         return (
             <>
                 <Header history={this.props.history}/>
                 <Box className={classes.margin}>
-                    <center><h1>{this.props.user.username}'s Progress</h1></center>
+                    <center><h1>Fitness at a Glance</h1></center>
+
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Name</TableCell>
+                                    <TableCell align="right">Date</TableCell>
+                                    <TableCell align="right">Weight</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow key={row.name}>
+                                        <TableCell component="th" scope="row">{row.name}</TableCell>
+                                        <TableCell align="right">{row.date}</TableCell>
+                                        <TableCell align="right">{row.weight}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+
+
                 </Box>
             </>
         )
